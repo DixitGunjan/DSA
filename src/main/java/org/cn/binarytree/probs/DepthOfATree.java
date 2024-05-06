815e7c566cb355b0f705799088a4e70047db69fa
DixitGunjan98 <gunjan.dixit@deltacapita.com>
2024-05-06 22:09:42 +0530

    Added tree classes

--- src/main/java/org/cn/binarytree/probs/DepthOfATree.java
+++ src/main/java/org/cn/binarytree/probs/DepthOfATree.java
@@ -6,20 +6,13 @@ import org.cn.binarytree.basics.TakeInputLevelWise;
 public class DepthOfATree {
 
     public static int height(BinaryTreeNode<Integer> root) {
-        int leftHeight = 0;
-        int rightHeight = 0;
+
         if (root == null) return 0;
 
-        if (root.left != null) {
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
 
-            leftHeight += height(root.left);
-        }
-        if (root.right != null) {
-            rightHeight += height(root.right);
-        }
-
-        if(leftHeight > rightHeight) return leftHeight + 1;
-        else return rightHeight + 1;
+        return 1 + Math.max(leftHeight, rightHeight);
     }
 
     public static void main(String[] args) {
