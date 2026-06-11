5d93bd15848a9506d7309636d45cb56fa73a47a6
Gunjan Dixit <38187091+DixitGunjan@users.noreply.github.com>
2026-06-11 21:14:20 +0530

    practise

--- src/main/java/org/binary_search/FirstAndLastPositionInSortedArray.java
+++ src/main/java/org/binary_search/FirstAndLastPositionInSortedArray.java
@@ -60,4 +60,27 @@ public class FirstAndLastPositionInSortedArray {
         }
         return result;
     }
+
+
+    private static int binarySearch(int[] arr, int x, boolean leftS) {
+        int result = -1;
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] > x) {
+                right = mid - 1;
+            } else if (arr[mid] < x) {
+                left = mid + 1;
+            } else {
+                result = mid;
+                if (leftS)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+
+        }
+        return result;
+    }
 }
