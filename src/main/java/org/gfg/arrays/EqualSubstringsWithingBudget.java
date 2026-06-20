15783cc01db4f7b9e9654a3271057f3af6a0667e
Gunjan Dixit <38187091+DixitGunjan@users.noreply.github.com>
2026-06-20 21:11:11 +0530

    practise

---
+++ src/main/java/org/gfg/arrays/EqualSubstringsWithingBudget.java
@@ -0,0 +1,32 @@
+package org.gfg.arrays;
+
+public class EqualSubstringsWithingBudget {
+    void main() {
+        String s = "abcd";
+        String t = "defg";
+        int cost = 3;
+
+        System.out.println(equalSubstring(s, t, cost));
+    }
+
+    private int equalSubstring(String s, String t, int cost) {
+        if (cost == 0) return 1;
+        int l = 0, r = 0;
+
+        int result = 0;
+        int currXost = 0;
+
+        while (r < s.length()) {
+            currXost += Math.abs(s.charAt(r) - t.charAt(r));
+
+            while (currXost > cost) {
+                currXost -= Math.abs(s.charAt(l) - t.charAt(l));
+                l++;
+            }
+
+            result = Math.max(result, r - l + 1);
+            r++;
+        }
+        return result;
+    }
+}
